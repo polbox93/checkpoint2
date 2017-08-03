9828bbe1daf40ad25a2ac8e94d36d652b8dc659a
polbox93 <polbox93@gmail.com>
2017-08-04 00:02:41 +0200

    Add sort method and create_from_csv static method in Address_book class

--- address_book.py
+++ address_book.py
@@ -26,3 +26,45 @@ class AddressBook():
                 results_list.append(element)
 
         return results_list
+
+
+    def sort(self):
+
+        for element in range(len(self.addresses)-1,0,-1):
+            for i in range(len(self.addresses)-1):
+                if self.addresses[i].get_full_address() > self.addresses[i+1].get_full_address():
+                    temp = self.addresses[i]
+                    self.addresses[i] = self.addresses[i+1]
+                    self.addresses[i+1] = temp
+
+        return self.addresses
+
+    @staticmethod
+    def load_items_from_file(file_name):
+
+        if file_name:
+            with open (file_name, 'r') as csv_file:
+                list_of_items = reader(csv_file, delimiter = ',')
+                item_list = []
+                for row in list_of_items:
+                    item_list.append(row)
+            return item_list
+        else:
+            raise FileNotFoundError
+
+    @staticmethod
+    def create_from_csv(list_name, csv_path):
+        database_list = AddressBook.load_items_from_file(csv_path)
+        database_list.remove(database_list[0])
+
+        book = AddressBook(list_name)
+
+        for element in database_list:
+            if element[4] == '':
+                address = Address(element[0], element[1], element[2], element[3])
+                book.add_address(address)
+            else:
+                address = WorkAddress(element[0], element[1], element[2], element[3], element[4])
+                book.add_address(address)
+
+        return book
